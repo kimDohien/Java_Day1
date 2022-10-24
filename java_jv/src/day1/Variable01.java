cef106723a0c5aea54d19fb5fb8664bd08b5f49e
dohyeon <koolove00221177@gmail.com>
2022-10-24 21:37:41 +0900

    자바01차_변수선언

---
+++ java_jv/src/day1/Variable01.java
@@ -0,0 +1,46 @@
+package day1;
+
+public class Variable01 {
+
+	public static void main(String[] args) {
+		/* - 변수선언
+		 * 자료형 변수형;
+		 * 자료형 변수형 = 초기값;
+		 * - 자료형
+		 */
+		boolean isEven = false;
+		char operator = 'a';
+		int age = 25;
+		float pi1 = 3.1415f; //float은 접미사 F 또는 f를 붙여줘야한다.
+		double pi2 = 3.1415;
+		// 참조형 문자열
+		String str = "+"; //문자가 꼭 들어가있어야함. 공백X
+		
+		
+		//사과의 갯수를 저장하기 위한 변수를 선언해라
+		int appleCount; 
+		
+		//학생 평균성적을 저장하기 위한 변수를 선언해
+		double scoreAverage;
+		
+		int num;
+		//int num; Duplicate local variable num:중독된 변수를 선언했기에 오류가 난거임
+		int NUM;
+		int num1;
+		//int 1num;숫자로 시작할수 없음
+		int _num, $num;
+	//int !num2;//_와 $가 아닌 문자가 나옴
+		//int int;//예약어 사용
+		
+		/*관례 :안지켜도 되지만 많은 개발자들이 지킴
+		 * - 변수는 소문자로 시작
+		 * - 카멜 표기법을 따라서 두번째 글자부터 첫글자를 대문자로
+		 * - 클래스와 인터페이스는 대문자로 시작
+		**/
+		
+		
+		
+		
+	}
+
+}

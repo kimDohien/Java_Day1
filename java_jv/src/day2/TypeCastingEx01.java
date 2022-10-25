e7dc6d072f21f91134a6da448458a520cc1e43a8
dohyeon <koolove00221177@gmail.com>
2022-10-25 18:08:24 +0900

    자바 02일차_형변환 예제

---
+++ java_jv/src/day2/TypeCastingEx01.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class TypeCastingEx01 {
+
+	public static void main(String[] args) {
+		float num1 =(float) 1.23;
+		//정수 리터럴은 자료형에 맞는 타입으로 설정해야한다.
+		byte num2 = 2;
+		long num3 = 3;
+		int num4 = (int)12345678901L;
+		
+		
+	}
+
+}

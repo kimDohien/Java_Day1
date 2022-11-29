273b9c602f62cfa59c2fe2e687bc81e8355371ba
dohyeon <koolove00221177@gmail.com>
2022-11-29 20:07:39 +0900

    자바 27일차_쓰레드(Thread) 예제

---
+++ java_jv/src/day27/ThreadEx04.java
@@ -0,0 +1,38 @@
+package day27;
+
+public class ThreadEx04 {
+
+	public static void main(String[] args) {
+		//<람다식 활용>
+
+	Thread t= new Thread(()->{
+			for(int i =0 ; i<10; i++) {//10번출력
+				//콘솔에 쓰레드 이름과 쓰레드실행을 1초마다 한번씩 찍어줌(10번 될때까지)
+				System.out.println("쓰레드 실행"); 
+				try {
+					Thread.sleep(1000);//1000ms = 1초간 멈춘다.
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} 
+			}	
+			
+		}
+	);
+
+	t.start(); //run이라는 메소드를 호출
+	Thread thisThread = Thread.currentThread();
+	for(int i =0; i<10 ; i++) {
+		System.out.println(thisThread.getName()+"쓰레드 실행");
+		try {
+			Thread.sleep(400);//1000ms = 1초간 멈춘다.
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} 
+		
+	}
+	
+	}
+
+}
+
+

6986d4ae45317066cf5885f411c0b8ad632b2f77
dohyeon <koolove00221177@gmail.com>
2022-12-28 20:47:52 +0900

    JDBC_04일차_Example_Mybatis(Dao)

---
+++ java_jv/src/example/db/dao/StudentDAO.java
@@ -0,0 +1,28 @@
+package example.db.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import db.day3.vo.StudentVO;
+import example.db.StudentVO2;
+
+public interface StudentDAO {
+
+
+	public ArrayList<example.db.StudentVO> selectAllStudent();
+
+	public int insertStudent(example.db.StudentVO std);
+
+	public int updateStudent(example.db.StudentVO std);
+
+	public example.db.StudentVO selectStudent(String st_num);
+
+	//resultMap
+	public ArrayList<StudentVO2> selectAllStudent2(); 
+
+
+
+	
+}

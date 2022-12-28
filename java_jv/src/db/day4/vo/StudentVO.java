48280e8382139e1f6f4eb0b58478143d54c7b157
dohyeon <koolove00221177@gmail.com>
2022-12-28 20:45:07 +0900

    JDBC_04일차_Mybatis(VO)

---
+++ java_jv/src/db/day4/vo/StudentVO.java
@@ -0,0 +1,20 @@
+package db.day4.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class StudentVO {
+	
+	private String st_num;
+	private String st_name;
+	private int st_semester;
+	private String st_status;
+	private String st_pr_num;
+	
+
+
+}

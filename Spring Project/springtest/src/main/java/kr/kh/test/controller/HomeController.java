ebe2327ae8ab7ba28c2a5ba1cafca3d104aa75ef
dohyeon <koolove00221177@gmail.com>
2023-02-10 19:12:47 +0900

    Spring_3일차_스프링복습 - DB연동 완료

--- Spring Project/springtest/src/main/java/kr/kh/test/controller/HomeController.java
+++ Spring Project/springtest/src/main/java/kr/kh/test/controller/HomeController.java
@@ -6,17 +6,21 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import kr.kh.test.service.MemberService;
+
 
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@Autowired
+	MemberService memberService;
 	
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
